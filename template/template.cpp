b7b084100011911a4709c28a0b2ba45ffa511d76
flow_frenzy_27 <flow_frenzy_27@passwordisyouarepro.local>
2023-08-09 12:11:08 +0530

    first commit

---
+++ template/template.cpp
@@ -0,0 +1,30 @@
+#include<stdc++.h>
+using namespace std;
+
+int mod = 1000000007;
+void solve() {
+	
+}
+
+int32_t main()
+{
+
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen( "../input/input.txt", "r" , stdin);
+	freopen( "../output/output.txt" , "w" , stdout);
+#endif
+
+
+	clock_t z = clock();
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+	return 0;
+
+}
